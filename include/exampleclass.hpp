01b43aca895d9e19a3555cc36b49f429e71e91f7
Miguel Chicas <immac.gm@gmail.com>
2018-06-14 16:45:49 -0600

    Creates a new project from template

---
+++ include/exampleclass.hpp
@@ -0,0 +1,10 @@
+#ifndef EXAMPLE_CLASS_HPP
+#define EXAMPLE_CLASS_HPP
+
+class Example
+{
+    public:
+    int example = 100;
+};
+
+#endif // EXAMPLE_CLASS_HPP
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "exampleclass.hpp"
+
+int main(int argc, char const *argv[])
+{
+    Example example;
+    std::cout << example.example << std::endl;
+    return 0;
+}
